c4f76f9c070130990c89d6b2d827b6582a8ef570
Hrushi1509 <90896961+Hrushi1509@users.noreply.github.com>
2022-05-24 19:38:33 +0530

    abc

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,8 +4,8 @@ import React from "react";
 const Welcome = ({ name }) => {
   return (
     <>
-      Hey! <h1>{name}</h1>
-      <h2>Welcome to Newton School.</h2>
+      "Hey!"" <h1>{name}</h1>
+      <h2>"Welcome to Newton School."</h2>
     </>
   );
 };
